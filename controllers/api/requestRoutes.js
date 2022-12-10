380dee43d9404914d1d3c0a2cdf1e0aee65b3160
Janelle Cikanek <janellecikanek@gmail.com>
2022-12-10 11:05:43 -0600

    updated date

--- controllers/api/requestRoutes.js
+++ controllers/api/requestRoutes.js
@@ -15,6 +15,27 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+// update request as claimed
+// router.put('/api/requests/:requestId/giver/:userId', async (req, res) => {
+//   try {
+//     const requestData = await Request.update({
+//       request_title: req.body.request_title
+//     }, {
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+
+//     res.status(200).json(tagData);
+//     if (!userData) {
+//       res.status(404).json({ message: 'No request with this id!' });
+//       return;
+//     }
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const requestData = await Request.destroy({
