52a932cee0962dc2e86d2dab5fc25669a048a0ae
wolfjfly <104739255+wolfjfly@users.noreply.github.com>
2022-12-15 06:17:39 -0600

    added /singUp to routes

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -6,7 +6,7 @@ $("#button").click(async function(event){
 
     if (name && email && password) {
       // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/singUp', {
+      const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({ name, email, password }),
         headers: { 'Content-Type': 'application/json' },
